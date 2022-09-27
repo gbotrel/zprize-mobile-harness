21e38e1f5074fc5f03387b509d27b3f9e000d55a
Gautam Botrel <gautam.botrel@gmail.com>
2022-09-27 16:12:28 -0500

    feat: restore zprize tests

--- msm/bls12-377/zprize_test.go
+++ msm/bls12-377/zprize_test.go
@@ -1,10 +1,80 @@
 package bls12377
 
 import (
+	"bytes"
+	"os"
+	"testing"
+
+	"github.com/consensys/gnark-crypto/ecc"
 	"github.com/gbotrel/zprize-mobile-harness/msm/bls12-377/fp"
 	"github.com/gbotrel/zprize-mobile-harness/msm/bls12-377/fr"
 	"github.com/leanovate/gopter"
+	"github.com/stretchr/testify/require"
 )
+
+func TestMultiExp(t *testing.T) {
+	assert := require.New(t)
+	scalars, err := ReadScalars("scalars")
+	assert.NoError(err, "reading scalar file")
+	points, err := ReadPoints("points")
+	assert.NoError(err, "reading points file")
+	results, err := ReadResults("result")
+	assert.NoError(err, "reading results file")
+
+	assert.True(len(scalars) == len(points))
+	assert.True(len(scalars) == len(results))
+
+	// perform the msms
+	for i := 0; i < len(scalars); i++ {
+		for J := 0; J < len(scalars[i]); J++ {
+			scalars[i][J].FromMont()
+		}
+		var p G1Affine
+		_, err = p.MultiExp(points[i], scalars[i], ecc.MultiExpConfig{})
+		assert.NoError(err)
+
+		assert.True(p.Equal(&results[i]), "msm mismatch")
+	}
+}
+
+func TestSerializationScalars(t *testing.T) {
+	assert := require.New(t)
+
+	scalars, err := ReadScalars("scalars")
+	assert.NoError(err, "reading scalar file")
+
+	bScalars := SerializeScalars(scalars)
+	fScalars, err := os.ReadFile("scalars")
+	assert.NoError(err, "reading scalar file 2nd time")
+
+	assert.True(bytes.Equal(bScalars, fScalars), "bad encoding of scalars")
+}
+
+func TestSerializationPoints(t *testing.T) {
+	assert := require.New(t)
+
+	points, err := ReadPoints("points")
+	assert.NoError(err, "reading points file")
+
+	bPoints := SerializePoints(points)
+	fPoints, err := os.ReadFile("points")
+	assert.NoError(err, "reading points file 2nd time")
+
+	assert.True(bytes.Equal(bPoints, fPoints), "bad encoding of points")
+}
+
+func TestSerializationResults(t *testing.T) {
+	assert := require.New(t)
+
+	points, err := ReadResults("result")
+	assert.NoError(err, "reading results file")
+
+	bPoints := SerializeResults(points)
+	fPoints, err := os.ReadFile("result")
+	assert.NoError(err, "reading results file 2nd time")
+
+	assert.True(bytes.Equal(bPoints, fPoints), "bad encoding of results")
+}
 
 const nbFuzzShort = 10
 const nbFuzz = 100
